42a00aad3e7f1108aa6821c160d7a6c50cd83fc2
naveenpanjala <naveenkumar12345k@gmail.com>
2024-04-15 17:15:11 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import './App.css';
-import { Home } from './pages/Home';
-
+import React from "react";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import { Home } from "./pages/Home";
+import { LoginPage } from "./pages/LoginPage";
+import { SignupPage } from "./pages/SignupPage";
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home></Home>,
+    },
+    {
+      path: "/login",
+      element: <LoginPage></LoginPage>,
+    },
+    {
+      path: "/signup",
+      element: <SignupPage></SignupPage>,
+    },
+  ]);
   return (
-    <>
-   <Home></Home>
-    </>
-   
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
